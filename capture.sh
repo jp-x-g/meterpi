f1292029845addbc3619fb90cb2c58d875501b2e
jp-x-g <jpxg-dev@protonmail.com>
2026-01-06 04:23:46 -0800

    Add

--- capture.sh
+++ capture.sh
@@ -2,4 +2,7 @@ fn="$(date +'%F%H:%M:%S').jpg"
 
 mkdir data
 
-sudo fswebcam -d /dev/video0 -S 20 --flip h data/"$fn" && 
+sudo fswebcam -d /dev/video0 -S 20 --flip h data/"$fn"
+IFS=: read -r u p < auth.txt 
+curl -T "$fn" -u "$(cat auth.txt)" "ftp://phoeni.city/"
+
